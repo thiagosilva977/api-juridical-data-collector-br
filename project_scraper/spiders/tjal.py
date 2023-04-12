421656d94051648ff4eaaf9411f22389f92e4796
thiagosilva977 <thiagosilva977@hotmail.com>
2023-04-12 10:15:13 -0300

    parsed partes do processo

--- project_scraper/spiders/tjal.py
+++ project_scraper/spiders/tjal.py
@@ -22,6 +22,10 @@ class TjalSpider(scrapy.Spider):
                               f'processo.numero={self._input_url}')
                           ]
 
+        urls_to_scrape = [str(f'https://www2.tjal.jus.br/cpopg/show.do?'
+                              f'processo.numero={self._input_url}'),
+                          ]
+
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -55,34 +59,81 @@ class TjalSpider(scrapy.Spider):
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        print(soup)
+        try:
+            data_classe = soup.find('span', {'id': 'classeProcesso'})['title']
+            print(data_classe)
+        except TypeError:
+            pass
+        try:
+            data_assunto = soup.find('span', {'id': 'assuntoProcesso'})['title']
+            print(data_assunto)
+        except TypeError:
+            pass
 
-        data_classe = soup.find('span', {'id': 'classeProcesso'})['title']
-        print(data_classe)
+        try:
+            data_foro = soup.find('span', {'id': 'foroProcesso'})['title']
+            print(data_foro)
+        except TypeError:
+            pass
+        try:
+            data_vara = soup.find('span', {'id': 'varaProcesso'})['title']
+            print(data_vara)
+        except TypeError:
+            pass
+        try:
+            data_juiz = soup.find('span', {'id': 'juizProcesso'})['title']
+            print(data_juiz)
+        except TypeError:
+            pass
+        try:
+            data_area = soup.find('div', {'id': 'areaProcesso'}).find_next('span').text
+            print(data_area)
+        except TypeError:
+            pass
 
-        data_assunto = soup.find('span', {'id': 'assuntoProcesso'})['title']
-        print(data_assunto)
+        try:
+            data_distribuicao = soup.find('div', {'id': 'dataHoraDistribuicaoProcesso'}).text
+            print(data_distribuicao)
+        except TypeError:
+            pass
+        try:
+            data_valor = soup.find('div', {'id': 'valorAcaoProcesso'}).text
+            print(data_valor)
+        except TypeError:
+            pass
 
-        data_foro = soup.find('span', {'id': 'foroProcesso'})['title']
-        print(data_foro)
+        table_partes = soup.find('table', {'id': 'tableTodasPartes'})
 
-        data_vara = soup.find('span', {'id': 'varaProcesso'})['title']
-        print(data_vara)
+        processos = []
+        for tr in table_partes.find_all("tr", class_="fundoClaro"):
+            tipo_autor = tr.find("td", class_="label").text.strip()
+            nome_parte = tr.find("td", class_="nomeParteEAdvogado").text.split('Advogado:')[0].strip()
+            advogados = []
+            # test = tr.find_all('span', {'class': 'mensagemExibindo'})
 
-        data_juiz = soup.find('span', {'id': 'juizProcesso'})['title']
-        print(data_juiz)
+            list_split = tr.text.split('Advogado:')
+            print(list_split)
+            print(len(list_split))
+            for i in range(len(list_split)):
+                if i == 0:
+                    pass
+                else:
+                    current_text = list_split[i].replace('\n', '').replace('\t', '').replace('\xa0', '').strip()
+                    if 'Advogada:' in current_text:
+                        current_text = current_text.split('Advogada:')
+                        for item in current_text:
 
-        data_area = soup.find('div', {'id': 'areaProcesso'}).find_next('span').text
-        print(data_area)
+                            advogados.append(item)
+                    else:
+                        advogados.append(current_text)
 
-        data_distribuicao = soup.find('div', {'id': 'dataHoraDistribuicaoProcesso'}).text
-        print(data_distribuicao)
+            processo = {
+                "tipo_autor": tipo_autor,
+                "nome_parte": nome_parte,
+                "nome_advogados": advogados
+            }
+            processos.append(processo)
 
-        data_valor = soup.find('div', {'id': 'valorAcaoProcesso'}).text
-        print(data_valor)
-
-
-        
 
         print('finishh')
         time.sleep(1212)
